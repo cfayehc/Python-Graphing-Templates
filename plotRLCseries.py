b108385aceccf76d676508db663127b55838de9e
cfayehc <37842767+cfayehc@users.noreply.github.com>
2021-11-01 13:16:36 -0400

    Add files via upload
    Plotting resonances of RLC circuits from the formulae given in lab 3 of electronics...They will also plot data

---
+++ plotRLCseries.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 21 15:45:53 2020
+
+@author: cfay
+"""
+
+#function definition
+
+#main
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import csv
+
+x=[]
+y=[]
+y1=[]
+y2=[]
+xdata=[]
+ydata=[]
+
+with open('data.csv', 'r') as csvfile:
+    plots= csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        xdata.append(float(row[0]))
+        ydata.append(float(row[1]))
+
+#p1=plt.scatter(xdata,ydata, marker='o')
+
+#vector = np.vectorize(np.float)
+x = np.arange(500, 100000, .50)
+#x=np.float(xp)
+#x = np.array(list(map(np.float, x)))
+#x = vector(x)
+R = 100.
+C = .1E-6
+L = 10.0E-3
+#y = (x*L)/(R**2*(1-x**2*L*C)**2+(x*L)**2)**(1/2)
+y1=(R)**2+(x*L-1/(x*C))**2
+y1 = np.array(list(map(np.float, y1)))
+y2=1/(x*C)
+y2 = np.array(list(map(np.float, y2)))
+y = (y2)/(np.sqrt(y1))
+
+print('Frequency: ', x)
+print('Gain: ', y)
+ 
+
+plt.grid(b=True, which='both', color='0.65', linestyle='-')
+plt.plot(x, y)
+ 
+plt.title("Resonance in series RLC circuit")
+plt.xlabel("Frequency")
+plt.ylabel("Gain")
+plt.show()
+
